26f6dd44da2105c80a468200d1df8c312aebd454
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-06-25 16:17:47 +0200

    Made views for recommender

--- schoolsRecommender/schools/urls.py
+++ schoolsRecommender/schools/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('recommend/', views.recommend_view, name='recommend'),
+    path('school/<int:school_id>/', views.school_detail_view, name='school_detail'),
 ]
--- schoolsRecommender/schools/views.py
+++ schoolsRecommender/schools/views.py
@@ -1,5 +1,25 @@
-from django.http import HttpResponse
+from .recommender import recommend_schools
+from django.shortcuts import render, get_object_or_404
+from .models import School
 
+def recommend_view(request):
+    if request.method == 'GET' and 'latitude' in request.GET and 'longitude' in request.GET:
+        latitude = float(request.GET['latitude'])
+        longitude = float(request.GET['longitude'])
+        recommendations = recommend_schools(latitude, longitude)
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the schools index.")
+        context = {
+            'recommendations': recommendations,
+        }
+        print(recommendations)
+
+        return render(request, 'schools/recommendations.html', context)
+
+    return render(request, 'schools/recommend_form.html')
+
+def school_detail_view(request, school_id):
+    school = get_object_or_404(School, id=school_id)
+    context = {
+        'school': school,
+    }
+    return render(request, 'schools/school_detail.html', context)
