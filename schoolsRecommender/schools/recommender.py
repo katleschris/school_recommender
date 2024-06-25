779c61b3fa66cdab4ec5c504406fccd547c20c68
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-06-25 16:20:57 +0200

    created script to handle rwcommender logic

---
+++ schoolsRecommender/schools/recommender.py
@@ -0,0 +1,20 @@
+from geopy.distance import geodesic
+from schools.models import School
+
+def recommend_schools(latitude, longitude, max_distance_km=50, top_n=50):
+    all_schools = School.objects.all()
+    recommendations = []
+
+
+    for school in all_schools:
+        print('school:', school.school_name)
+        distance = geodesic((latitude, longitude), (school.latitude, school.longitude)).km
+        distance = round(distance, 2)
+        if distance <= max_distance_km:
+            recommendations.append((school, distance))
+
+    # sort by distance
+    recommendations.sort(key=lambda x: x[1])
+    
+
+    return recommendations[:top_n]
