84c4b2047951ddf08f2f5c7428ed6c84fe85ef7b
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-06-27 12:23:12 +0200

    Improved code layout

--- schoolsRecommender/schools/recommender.py
+++ schoolsRecommender/schools/recommender.py
@@ -1,25 +1,3 @@
-# from geopy.distance import geodesic
-# from schools.models import School
-
-# def recommend_schools(latitude, longitude, max_distance_km=50, extended_distance_km=100, top_n=20):
-#     all_schools = School.objects.all()
-#     recommendations = []
-#     extended_recommendations = []
-
-#     for school in all_schools:
-#         distance = geodesic((latitude, longitude), (school.latitude, school.longitude)).km
-#         distance = round(distance, 2)
-#         if distance <= max_distance_km:
-#             recommendations.append((school, distance))
-#         elif distance <= extended_distance_km:
-#             extended_recommendations.append((school, distance))
-
-#     recommendations.sort(key=lambda x: x[1])
-#     extended_recommendations.sort(key=lambda x: x[1])
-
-#     return recommendations[:top_n], extended_recommendations[:top_n]
-
-
 from geopy.distance import geodesic
 from schools.models import School
 
